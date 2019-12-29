85ddf7387b8d64f0134c372e1d913617dd0672f4
ghost-guest <2661471268@qq.com>
2019-12-29 15:56:10 +0800

    third commits 20191229

--- test.py
+++ test.py
@@ -8,9 +8,22 @@
 # 说明： 
 
 from random import sample
-a = range(1000)
-print(a)
-b = sample(a, 5)
+from os import listdir
+import os
+# a = range(1000)
+# print(a)
+# b = sample(a, 5)
+# print(b)
+# dir_l = listdir('/Users/mac/Desktop/python')
+# fns = [fn for fn in dir_l if fn.endswith('.xlsx')]
+# print(fns)
 
 
-print(b)
+# 获取某个目录下的所有的execl的文件的路径，包括多层目录
+f_list = []
+for root, dirs, files in os.walk('/Users/mac/Desktop/python'):
+    for file in files:
+        f = os.path.join(root, file)
+        if f.endswith('.xlsx'):
+            f_list.append(f)
+print(f_list)
